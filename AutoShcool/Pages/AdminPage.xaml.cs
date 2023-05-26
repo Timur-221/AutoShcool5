aa0e6103a653a0e0f9da0ad6f44d644de6a32dc1
212007 <212007@1220-008-22>
2023-05-26 13:19:18 +0300

    Add project files.

---
+++ AutoShcool/Pages/AdminPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AutoShcool.DB;
+using AutoShcool.MyClass;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace AutoShcool.Pages
+{
+    /// <summary>
+    /// Interaction logic for AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : Page
+    {
+        MonD Mon { get; set; }
+
+        UserInfo UserInfo { get; set; }
+        public AdminPage()
+        {
+            InitializeComponent();
+            Mon = new MonD();
+            UserInfo = new UserInfo();
+        }
+
+        private void ExNav(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new AuthPage());
+        }
+
+        private void AddStud(object sender, RoutedEventArgs e)
+        {
+            UserInfo.Login = usernameTextBox.Text;
+            UserInfo.Password = passwordBox.Password;
+            UserInfo.NumGroup = GroupText.Text;
+            UserInfo.Category = CategoryText.Text;
+            UserInfo.Name = NameText.Text;
+            UserInfo.FName = FNameText.Text;
+            UserInfo.Otchestvo = OtcText.Text;
+
+            Mon.AddUser(UserInfo);
+            MessageBox.Show("Добавлен Ученик!");
+
+            usernameTextBox.Text = string.Empty;
+            passwordBox.Password = string.Empty;
+            GroupText.Text = string.Empty;
+            CategoryText.Text = string.Empty;
+            NameText.Text = string.Empty;
+            FNameText.Text = string.Empty ;
+            OtcText.Text = string.Empty ;
+        }
+
+
+        private void LoadNamesFromMongoDB(object sender, RoutedEventArgs e)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase("AutoSchool");
+            var collection = database.GetCollection<BsonDocument>("Student");
+
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var projection = Builders<BsonDocument>.Projection.Include("Name").Include("FName");
+
+            var cursor = collection.Find(filter).Project(projection).ToCursor();
+
+            List<string> names = new List<string>();
+            foreach (var document in cursor.ToEnumerable())
+            {
+                string firstName = document.GetValue("Name", "").AsString;
+                string lastName = document.GetValue("FName", "").AsString;
+
+                string fullName = $"{firstName} {lastName}";
+                names.Add(fullName);
+            }
+
+            userListBox.ItemsSource = names;
+        }
+    }
+}
---
+++ AutoShcool/Pages/ProfPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using AutoShcool.MyClass;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AutoShcool.DB;
+using AutoShcool.MyClass;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace AutoShcool.Pages
+{
+    /// <summary>
+    /// Interaction logic for ProfPage.xaml
+    /// </summary>
+    public partial class ProfPage : Page
+    {
+        public ProfPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AccNav(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new AccStud());
+        }
+
+        private void ExNAv(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new AuthPage());
+        }
+
+        //private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    var connectionString = "mongodb://localhost:27017";
+        //    var client = new MongoClient(connectionString);
+        //    var database = client.GetDatabase("AutoSchool");
+        //    var collection = database.GetCollection<BsonDocument>("Student");
+
+        //    var filter = Builders<BsonDocument>.Filter.Eq("Name", "ваше имя");
+        //    var document = collection.Find(filter).FirstOrDefault();
+
+        //    var name = document["Name"].ToString();
+
+        //    Nametext.Text = name;
+        //}
+    }
+}
